301a86b328af6b3fc51aa8b6992f4fb3fc7521da
Sanjee <sanjee.yogeswaran@gmail.com>
2022-01-11 09:36:02 -0600

    bugfix

--- scraper/sql_inserts.py
+++ scraper/sql_inserts.py
@@ -33,7 +33,7 @@ async def billProcessor(billList, congressNumber, table, session, pool):
                 filename = f'/congress/data/{congressNumber}/bills/{table.__tablename__}/{b}/data.json'
                 async with aiofiles.open(filename, mode='r') as f:
                     contents = await f.read()
-                    task = await asyncio.wrap_future(pool.submit(process, contents, congressNumber, table, session, pool, billFormat='json'))
+                    task = await asyncio.wrap_future(pool.submit(process, contents, congressNumber, table, session, billFormat='json'))
                     tasks.append(task)
         except:
             traceback.print_exc()
